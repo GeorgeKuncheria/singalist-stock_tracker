8c50d10ab994344ac9cd9232b0966366bf7d3d78
George Chempumthara <chempumthara.g@northeastern.edu>
2026-04-25 05:30:19 -0400

    Header and buttons

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation"
+import { Button } from "./ui/button";
+import { Avatar,AvatarFallback,AvatarImage } from "./ui/avatar";
+
+const UserDropdown = () => {
+  
+    const router=useRouter();
+
+    const handleSignOut= async ()=>{
+        // Clear user session or token here
+        // Then redirect to login page
+        router.push('/sign-in');
+    }
+
+    const user = {
+        name: "John Doe",
+        email: "contact@gmail.com"
+    }
+
+
+    return (
+
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+                <Avatar className="h-8 w-8">
+                    <AvatarImage src="https://toppng.com/uploads/preview/batman-logo-115309572496elqb1t51h.png" />
+                    <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold '>
+                        {user.name[0]}
+                    </AvatarFallback>
+                </Avatar>
+                <div className="hidden md:flex flex-col items-start">
+                    <span className="text-base font-medium text-gray-400">
+                        {user.name} 
+                    </span>
+                </div>
+            </Button>
+        </DropdownMenuTrigger>
+
+
+        <DropdownMenuContent className="text-gray-400 w-64">
+            <DropdownMenuGroup>          
+                <DropdownMenuLabel>
+                <div className="flex relative items-center gap-3 py-2">
+                    <Avatar className="h-10 w-10">
+                    <AvatarImage src="https://toppng.com/uploads/preview/batman-logo-115309572496elqb1t51h.png" />
+                    <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
+                        {user.name[0]}
+                    </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col">
+                    <span className="text-base font-medium text-gray-400">
+                        {user.name}
+                    </span>
+                    <span className="text-sm text-gray-500">
+                        {user.email}
+                    </span>
+                    </div>
+                </div>
+                </DropdownMenuLabel>
+            </DropdownMenuGroup>
+
+            <DropdownMenuSeparator className="bg-gray-600" />
+                {/* Focus here logout button */}
+        
+        
+        </DropdownMenuContent>
+
+
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdown
