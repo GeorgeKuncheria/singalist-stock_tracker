34b2f1c612a1522ac6fb2040d203e8b5e27eca20
George Chempumthara <chempumthara.g@northeastern.edu>
2026-04-26 14:36:33 -0400

    Implement auth ui and layout

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -34,7 +34,7 @@ const UserDropdown = () => {
     return (
 
     <DropdownMenu>
-        <DropdownMenuTrigger>
+        {/* <DropdownMenuTrigger>
             <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
                 <Avatar className="h-8 w-8">
                     <AvatarImage src="https://toppng.com/uploads/preview/batman-logo-115309572496elqb1t51h.png" />
@@ -48,6 +48,20 @@ const UserDropdown = () => {
                     </span>
                 </div>
             </Button>
+        </DropdownMenuTrigger> */}
+
+        <DropdownMenuTrigger className="flex items-center gap-3 text-gray-400 hover:text-yellow-500 cursor-pointer">
+            <Avatar className="h-8 w-8">
+                <AvatarImage src="https://toppng.com/uploads/preview/batman-logo-115309572496elqb1t51h.png" />
+                <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
+                {user.name[0]}
+                </AvatarFallback>
+            </Avatar>
+            <div className="hidden md:flex flex-col items-start">
+                <span className="text-base font-medium text-gray-400">
+                {user.name}
+                </span>
+            </div>
         </DropdownMenuTrigger>
 
 
